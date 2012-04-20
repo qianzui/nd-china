32f4581adc11b87f8a4190e4d764cd1f82e52612
fuweiwei0001@gmail.com <fuweiwei0001@gmail.com@a794dee8-fd84-c021-6015-35cb739101f1>
2012-04-20 10:43:58 +0000

    修改刷新间隔优化占用

--- trunk/Spearhead/src/com/hiapk/spearhead/Main.java
+++ trunk/Spearhead/src/com/hiapk/spearhead/Main.java
@@ -4,8 +4,8 @@ import java.text.DecimalFormat;
 import java.util.List;
 
 import com.hiapk.broadcreceiver.AlarmSet;
+import com.hiapk.progressbar.MyProgressBar;
 import com.hiapk.progressbar.ProgressBarForV;
-import com.hiapk.progressbar.myProgressBar;
 import com.hiapk.sqlhelper.SQLHelperTotal;
 import com.hiapk.sqlhelper.SQLHelperUid;
 
@@ -128,23 +128,27 @@ public class Main extends Activity {
 	 */
 	private void RefreshProgressBar(int i,int j) {
 //		ProgressBar myProgressBar = (ProgressBar) findViewById(R.id.progressbar);
-		myProgressBar myProgressBar2 = (myProgressBar) findViewById(R.id.progressbar);
-//		myProgressBar.setProgress(100);
-		ProgressBarForV progforv= new  ProgressBarForV();
-//		progforv.j=100-j;
-		progforv.j=20;
-		progforv.execute(myProgressBar2);
+		MyProgressBar myProgressBar_mobile = (MyProgressBar) findViewById(R.id.progressbar_mobile);
+		MyProgressBar myProgressBar_wifi = (MyProgressBar) findViewById(R.id.progressbar_wifi);
+		ProgressBarForV progforv_mobile= new  ProgressBarForV();
+		progforv_mobile.j=i;
+		progforv_mobile.execute(myProgressBar_mobile);
+		ProgressBarForV progforv_wifi= new  ProgressBarForV();
+		progforv_wifi.j=j;
+		progforv_wifi.execute(myProgressBar_wifi);
 	}
 	/**
-	 * 执行动态进度条设置
+	 * 进度条设置
 	 * @param i
 	 * 移动数据
 	 * @param j
 	 * wifi
 	 */
 	private void ProgressBarSet(int i,int j) {
-		ProgressBar myProgressBar = (ProgressBar) findViewById(R.id.progressbar);
-		myProgressBar.setProgress(20);
+		MyProgressBar myProgressBar_mobile = (MyProgressBar) findViewById(R.id.progressbar_mobile);
+		MyProgressBar myProgressBar_wifi = (MyProgressBar) findViewById(R.id.progressbar_wifi);
+		myProgressBar_mobile.setProgress(i);
+		myProgressBar_wifi.setProgress(j);
 	}
 
 	/**
@@ -174,21 +178,20 @@ public class Main extends Activity {
 		// TODO Auto-generated method stub
 		super.onResume();
 		AlarmSet alset = new AlarmSet();
+		// 初始化网络状态
 		sqlhelperTotal.initTablemobileAndwifi(context);
 		if (SQLHelperTotal.TableWiFiOrG23 != ""
 				&& sqlhelperTotal.getIsInit(context)) {
 			// 启动闹钟
-			alset.StartAlarm(context);
-			// 初始化网络状态
-			sqlhelperTotal.initTablemobileAndwifi(context);
+			alset.StartAlarmMobile(context);
 			// 进行数据记录
 			sqlhelperTotal.RecordTotalwritestats(context, false);
 		} else if (SQLHelperTotal.TableWiFiOrG23 != "") {
+			alset.StartAlarmMobile(context);
 			sqlhelperTotal.initTablemobileAndwifi(context);
-			alset.StartAlarm(context);
 		}
 		initValues();
-		RefreshProgressBar(80,90);
+		RefreshProgressBar(0,0);
 	}
 
 	private void showlog(String string) {
