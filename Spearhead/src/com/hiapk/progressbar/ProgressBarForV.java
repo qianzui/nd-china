6ecbb80d71c9980246e85dcc6f8c839ea4e39e75
fuweiwei0001 <fuweiwei0001@gmail.com>
2012-04-20 10:43:19 +0000

    显示

--- Spearhead/src/com/hiapk/progressbar/ProgressBarForV.java
+++ Spearhead/src/com/hiapk/progressbar/ProgressBarForV.java
@@ -11,13 +11,13 @@ import android.widget.RemoteViews;
 
 public class ProgressBarForV extends AsyncTask<ProgressBar , ProgressBar, Integer> {
 	ProgressBar proBar;
-	private int i=100;
-	public int j=20;
+	private int i=0;
+	public int j=50;
 	protected Integer doInBackground(ProgressBar... params) {
 		// TODO Auto-generated method stub
 //		proBar=params[0];
 //		params[0].setProgress(10);
-		for (; i > j; i--) {
+		for (; i < j; i++) {
 			publishProgress(params[0]);
 		}
 		
@@ -28,13 +28,13 @@ public class ProgressBarForV extends AsyncTask<ProgressBar , ProgressBar, Intege
 	protected void onProgressUpdate(ProgressBar... progress) {
 		progress[0].setProgress(i);
 //		progress[0].setBackgroundColor(Color.rgb(255-i*2, i*2, 0));
-		Log.d("onProgress", "progress:" );
+//		Log.d("onProgress", "progress:" );
 	}
 
 	@Override
 	protected void onPostExecute(Integer result) {
 //		proBar.setProgress(result);
-		Log.d("onProgress", "result:" + result);
+//		Log.d("onProgress", "result:" + result);
 	}
 
 	
