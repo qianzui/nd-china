ae35cb280f95ae060c882787ec0e96d2320cca40
fuweiwei0001 <fuweiwei0001@gmail.com>
2012-04-23 06:55:58 +0000

    启动程序时进行数据记录并进行跳转

--- Spearhead/src/com/hiapk/spearhead/Splash.java
+++ Spearhead/src/com/hiapk/spearhead/Splash.java
@@ -1,27 +1,55 @@
 package com.hiapk.spearhead;
 
-import android.app.Activity;
-import android.content.Intent;
-import android.os.Bundle;
-import android.os.Handler;
+import com.hiapk.broadcreceiver.AlarmSet;
+import com.hiapk.sqlhelper.SQLHelperTotal;
+import com.hiapk.sqlhelper.SQLHelperUid;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
 
 public class Splash extends Activity {
-	  private final int SPLASH_DISPLAY_LENGHT = 5000; //延迟秒数  
-	  
-	    @Override 
-	    public void onCreate(Bundle savedInstanceState) { 
-	        super.onCreate(savedInstanceState); 
-	        setContentView(R.layout.splash); 
-	        new Handler().postDelayed(new Runnable(){ 
-	  
-	         @Override 
-	         public void run() { 
-	             Intent mainIntent = new Intent(Splash.this,SpearheadActivity.class); 
-	             Splash.this.startActivity(mainIntent); 
-	                 Splash.this.finish(); 
-	         } 
-	            
-	        }, SPLASH_DISPLAY_LENGHT); 
-	    } 
+	private Context context = this;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.splash);
+		new AsyncTaskonResume().execute(context);
 	}
+
+	private class AsyncTaskonResume extends
+			AsyncTask<Context, Integer, Integer> {
+		@Override
+		protected Integer doInBackground(Context... params) {
+			AlarmSet alset = new AlarmSet();
+			// 初始化网络状态
+			SQLHelperUid sqlhelperUid = new SQLHelperUid();
+			SQLHelperTotal sqlhelperTotal = new SQLHelperTotal();
+			sqlhelperTotal.initTablemobileAndwifi(context);
+			if (SQLHelperTotal.TableWiFiOrG23 != ""
+					&& sqlhelperTotal.getIsInit(context)) {
+				// 启动闹钟
+				alset.StartAlarm(context);
+				// 进行数据记录
+				sqlhelperUid.RecordUidwritestats(context, false);
+				sqlhelperTotal.RecordTotalwritestats(context, false);
+			} else if (SQLHelperTotal.TableWiFiOrG23 != "") {
+				alset.StartAlarm(context);
+				sqlhelperTotal.initTablemobileAndwifi(context);
+			}
+			return null;
+			// TODO Auto-generated method stub
+		}
+
+		@Override
+		protected void onPostExecute(Integer result) {
+			// TODO Auto-generated method stub
+			Intent mainIntent = new Intent(Splash.this, SpearheadActivity.class);
+			Splash.this.startActivity(mainIntent);
+			Splash.this.finish();
+		}
+	}
+}
