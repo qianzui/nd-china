676766de8bf444c6bda2d993d3da083a14808671
fuweiwei0001 <fuweiwei0001@gmail.com>
2012-04-24 11:23:40 +0000

    添加网络权限检查信息

--- Spearhead/src/com/hiapk/broadcreceiver/PackageReceiver.java
+++ Spearhead/src/com/hiapk/broadcreceiver/PackageReceiver.java
@@ -3,9 +3,11 @@ package com.hiapk.broadcreceiver;
 import com.hiapk.sqlhelper.SQLHelperTotal;
 import com.hiapk.sqlhelper.SQLHelperUid;
 
+import android.Manifest;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.database.sqlite.SQLiteDatabase;
 import android.util.Log;
@@ -24,23 +26,36 @@ public class PackageReceiver extends BroadcastReceiver {
 			// 监听包的安装
 			if (intent.getAction()
 					.equals("android.intent.action.PACKAGE_ADDED")) {
+				//检查为自身安装
 				if (packageNames
-						.equals("package:com.wind.androiddev.showtraffic")) {
+						.equals("package:com.hiapk.spearhead")) {
 					sqlhelperTotal.initTablemobileAndwifi(context);
-					// showLog("安装" + packageName[1]);
 				} else {
-					int uidnumber = 99999;
-					try {
-						uidnumber = context.getPackageManager().getPackageInfo(
-								packageName[1], getResultCode()).applicationInfo.uid;
-					} catch (NameNotFoundException e1) {
-						// TODO Auto-generated catch block
-						e1.printStackTrace();
-						showLog("获取包信息失败");
-					}
-					if (uidnumber != 99999) {
-						sqlhelperUid.updateSQLUidIndexOnInstall(context,
-								uidnumber, packageName[1], "Install");
+					//其他软件安装
+					//检测网络权限
+					PackageManager pkgmanager = context.getPackageManager();
+					if (PackageManager.PERMISSION_GRANTED != pkgmanager
+							.checkPermission(Manifest.permission.INTERNET,
+									packageName[1])) {
+						//无网络权限
+//						showLog("没网络权限的安装");
+					} else {
+						//有网络权限进行更新表格
+//						showLog("有网络权限的安装");
+						int uidnumber = 99999;
+						try {
+							uidnumber = context.getPackageManager()
+									.getPackageInfo(packageName[1],
+											getResultCode()).applicationInfo.uid;
+						} catch (NameNotFoundException e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+							showLog("获取包信息失败");
+						}
+						if (uidnumber != 99999) {
+							sqlhelperUid.updateSQLUidIndexOnInstall(context,
+									uidnumber, packageName[1], "Install");
+						}
 					}
 				}
 
@@ -49,7 +64,7 @@ public class PackageReceiver extends BroadcastReceiver {
 			if (intent.getAction().equals(
 					"android.intent.action.PACKAGE_REMOVED")) {
 				if (packageNames
-						.equals("package:com.wind.androiddev.showtraffic")) {
+						.equals("package:com.hiapk.spearhead")) {
 					sqlhelperTotal.initTablemobileAndwifi(context);
 					// 设置IsInit信息
 					sqlhelperTotal.getIsInit(context);
