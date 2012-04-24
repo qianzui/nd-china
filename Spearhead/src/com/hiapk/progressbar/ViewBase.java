fbe9c4e24db702f7833472233ef0b4b287d1ea9a
fuweiwei0001 <fuweiwei0001@gmail.com>
2012-04-24 11:22:28 +0000

    饼图的代码

---
+++ Spearhead/src/com/hiapk/progressbar/ViewBase.java
@@ -0,0 +1,35 @@
+/**
+ * @epic bin @2010
+ */
+package com.hiapk.progressbar;
+
+import android.content.Context;
+import android.view.View;
+
+/**
+ * @pro FinMobile ViewBase
+ * @uri view.ViewBase.java
+ * @author bin
+ * @mail/qq/msn ubin@vip.qq.com
+ * @version 1.0
+ * @date 2010-10-25 ÏÂÎç03:56:00
+ */
+public class ViewBase extends View {
+
+	public int width;
+	public int height;
+	
+	public ViewBase(Context context) {
+		super(context);
+	}
+	@Override
+	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+
+		height = View.MeasureSpec.getSize(heightMeasureSpec); 
+		width = View.MeasureSpec.getSize(widthMeasureSpec); 
+		setMeasuredDimension(width,height); 
+		
+		super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+	}
+
+}
