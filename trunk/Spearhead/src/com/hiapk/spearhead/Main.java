c5a313e43f245570b2e5031d13861a81bc768ea8
fuweiwei0001@gmail.com <fuweiwei0001@gmail.com@a794dee8-fd84-c021-6015-35cb739101f1>
2012-04-26 11:13:30 +0000

    添加柱状图显示

--- trunk/Spearhead/src/com/hiapk/spearhead/Main.java
+++ trunk/Spearhead/src/com/hiapk/spearhead/Main.java
@@ -7,6 +7,7 @@ import com.hiapk.broadcreceiver.AlarmSet;
 import com.hiapk.progressbar.MyProgressBar;
 import com.hiapk.progressbar.PieView;
 import com.hiapk.progressbar.ProgressBarForV;
+import com.hiapk.progressbar.StackedBarChart;
 import com.hiapk.sqlhelper.SQLHelperTotal;
 
 import android.app.Activity;
@@ -35,8 +36,17 @@ public class Main extends Activity {
 	private int[] uids;
 	private String[] packagenames;
 	// ------------
-	//显示何种图形
-	boolean ismobileshowpie=false;
+	// 显示何种图形
+	boolean ismobileshowpie = false;
+	// 获取的系统时间
+	private int year;
+	private int month;
+	private int monthDay;
+	private int weekDay;
+	// wifi月度流量
+	long[] wifiTraffic = new long[64];
+	long[] mobileTraffic = new long[64];
+
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -60,42 +70,40 @@ public class Main extends Activity {
 		TextView weekMobilunit = (TextView) findViewById(R.id.unit2);
 		TextView monthMobil = (TextView) findViewById(R.id.monthRate);
 		TextView monthMobilunit = (TextView) findViewById(R.id.unit3);
-		TextView todayWifi = (TextView) findViewById(R.id.wifiTodayRate);
-		TextView todayWifiunit = (TextView) findViewById(R.id.unit4);
-		TextView weekWifi = (TextView) findViewById(R.id.wifiWeekRate);
-		TextView weekWifiunit = (TextView) findViewById(R.id.unit5);
-		TextView monthWifi = (TextView) findViewById(R.id.wifiMonthRate);
-		TextView monthWifiunit = (TextView) findViewById(R.id.unit6);
-		long[] mobile = new long[64];
-		long[] wifi = new long[64];
+		// TextView todayWifi = (TextView) findViewById(R.id.wifiTodayRate);
+		// TextView todayWifiunit = (TextView) findViewById(R.id.unit4);
+		// TextView weekWifi = (TextView) findViewById(R.id.wifiWeekRate);
+		// TextView weekWifiunit = (TextView) findViewById(R.id.unit5);
+		// TextView monthWifi = (TextView) findViewById(R.id.wifiMonthRate);
+		// TextView monthWifiunit = (TextView) findViewById(R.id.unit6);
+		wifiTraffic = new long[64];
 		// 取得系统时间。
 		Time t = new Time();
 		t.setToNow();
-		int year = t.year;
-		int month = t.month + 1;
-		int monthDay = t.monthDay;
-		int weekDay = t.weekDay;
+		year = t.year;
+		month = t.month + 1;
+		monthDay = t.monthDay;
+		weekDay = t.weekDay;
 		// 取得每周的流量
 		long[] weektraffic = new long[6];
 		weektraffic = sqlhelperTotal.SelectWeekData(context, year, month,
 				monthDay, weekDay);
 		// 取得月度流量
-		mobile = sqlhelperTotal.SelectMobileData(context, year, month);
-		wifi = sqlhelperTotal.SelectWifiData(context, year, month);
+		mobileTraffic = sqlhelperTotal.SelectMobileData(context, year, month);
+		wifiTraffic = sqlhelperTotal.SelectWifiData(context, year, month);
 		// 进行流量设置
-		todayMobil.setText(unitHandler(
-				mobile[monthDay] + mobile[monthDay + 31], todayMobilunit));
+		todayMobil.setText(unitHandler(mobileTraffic[monthDay]
+				+ mobileTraffic[monthDay + 31], todayMobilunit));
 		weekMobil.setText(unitHandler(weektraffic[0], weekMobilunit));
-		mobile_month_use = mobile[0] + mobile[63];
+		mobile_month_use = mobileTraffic[0] + mobileTraffic[63];
 		monthMobil.setText(unitHandler(mobile_month_use, monthMobilunit));
-		todayWifi.setText(unitHandler(wifi[monthDay] + wifi[monthDay + 31],
-				todayWifiunit));
-		weekWifi.setText(unitHandler(weektraffic[5], weekWifiunit));
-		wifi_month_use = wifi[0] + wifi[63];
-		monthWifi.setText(unitHandler(wifi_month_use, monthWifiunit));
+		// todayWifi.setText(unitHandler(wifi[monthDay] + wifi[monthDay + 31],
+		// todayWifiunit));
+		// weekWifi.setText(unitHandler(weektraffic[5], weekWifiunit));
+		// wifi_month_use = wifi[0] + wifi[63];
+		// monthWifi.setText(unitHandler(wifi_month_use, monthWifiunit));
 
 	}
-
 
 	/**
 	 * 初始化数据库
@@ -143,7 +151,8 @@ public class Main extends Activity {
 		// ProgressBar myProgressBar = (ProgressBar)
 		// findViewById(R.id.progressbar);
 		MyProgressBar myProgressBar_mobile = (MyProgressBar) findViewById(R.id.progressbar_mobile);
-		MyProgressBar myProgressBar_wifi = (MyProgressBar) findViewById(R.id.progressbar_wifi);
+		// MyProgressBar myProgressBar_wifi = (MyProgressBar)
+		// findViewById(R.id.progressbar_wifi);
 		DisplayMetrics dm = new DisplayMetrics();
 		// 取得窗口属性
 		getWindowManager().getDefaultDisplay().getMetrics(dm);
@@ -151,13 +160,13 @@ public class Main extends Activity {
 		int fontsize = dm.widthPixels / 10;
 		// showlog(screenWidth+"");
 		myProgressBar_mobile.setTextsize(fontsize);
-		myProgressBar_wifi.setTextsize(fontsize);
+		// myProgressBar_wifi.setTextsize(fontsize);
 		ProgressBarForV progforv_mobile = new ProgressBarForV();
 		progforv_mobile.j = i;
 		progforv_mobile.execute(myProgressBar_mobile);
 		ProgressBarForV progforv_wifi = new ProgressBarForV();
-		progforv_wifi.j = j;
-		progforv_wifi.execute(myProgressBar_wifi);
+		// progforv_wifi.j = j;
+		// progforv_wifi.execute(myProgressBar_wifi);
 	}
 
 	/**
@@ -170,9 +179,10 @@ public class Main extends Activity {
 	 */
 	private void ProgressBarSet(int i, int j) {
 		MyProgressBar myProgressBar_mobile = (MyProgressBar) findViewById(R.id.progressbar_mobile);
-		MyProgressBar myProgressBar_wifi = (MyProgressBar) findViewById(R.id.progressbar_wifi);
+		// MyProgressBar myProgressBar_wifi = (MyProgressBar)
+		// findViewById(R.id.progressbar_wifi);
 		myProgressBar_mobile.setProgress(i);
-		myProgressBar_wifi.setProgress(j);
+		// myProgressBar_wifi.setProgress(j);
 	}
 
 	/**
@@ -227,33 +237,105 @@ public class Main extends Activity {
 		initValues();
 		initProgressBar();
 		initPieBar();
+		initWifiBar();
+	}
+
+	/**
+	 * 初始化wifi部分的柱状图
+	 */
+	private void initWifiBar() {
+		// TODO Auto-generated method stub
+		LinearLayout layout_mobile = (LinearLayout) findViewById(R.id.linearlayout_wifi);
+		StackedBarChart chartbar = initStackedBarChart(context);
+		View view = chartbar.execute(context);
+		layout_mobile.removeAllViews();
+		layout_mobile.addView(view);
+	}
+
+	/**
+	 * 设置wifi部分的柱状图
+	 * 
+	 * @param context
+	 * @return 返回显示的柱状图
+	 */
+	private StackedBarChart initStackedBarChart(Context context) {
+		StackedBarChart chartbar = new StackedBarChart(context);
+		// 进行参数设置
+		// 设置x轴显示范围
+		int monthtotalDay = countDay(year, month);
+		chartbar.setMonthDay(monthtotalDay);
+		// 设置y轴显示值及范围
+		double[] wifi = new double[monthDay];
+		long maxwifiTraffic = 0;
+		DecimalFormat format = new DecimalFormat("0.#");
+		// wifi[0] = (double) (wifiTraffic[0] + wifiTraffic[63]) / 1000000;
+		for (int i = 0; i < wifi.length; i++) {
+			long temp = wifiTraffic[i + 1] + wifiTraffic[i + 32];
+			// 小数点2位
+			wifi[i] = (double) ((int) temp / 10000) / 100;
+			// format.format(wifi[i]);
+			if (temp > maxwifiTraffic) {
+				maxwifiTraffic = temp;
+			}
+		}
+		chartbar.setData1(wifi);
+		chartbar.setMaxTraffic((double) (int) maxwifiTraffic / 1000000 * 1.2);
+		chartbar.setyMaxvalue((double) (int) maxwifiTraffic / 1000000 * 1.2);
+		// 设置背景色（被隐藏的条）
+		// chartbar.setBackgroundColor(Color.BLACK);
+		// 设置初始显示图像位置
+		if (monthDay + 2 < monthtotalDay) {
+			chartbar.setxMinvalue(monthDay - 5);
+			chartbar.setxMaxvalue(monthDay + 2);
+		} else {
+			chartbar.setxMinvalue(monthtotalDay - 7);
+			chartbar.setxMaxvalue(monthtotalDay);
+		}
+		// 设置显示的日期
+		String[] xaxles = new String[monthtotalDay];
+		for (int i = 0; i < monthtotalDay; i++) {
+			int j = i + 1;
+			xaxles[i] = month + "月" + j + "日";
+		}
+		chartbar.setXaxles(xaxles);
+		// showlog(monthDay+"");
+		return chartbar;
 	}
 
 	private void initPieBar() {
 		// TODO Auto-generated method stub
-		int[] percent = new int[] { 50, 140, 100, 70 };
-		final PieView pieView_mobile = new PieView(context,  percent);
-//		View PieView=findViewById(R.id.pie_bar_mobile);
-		LinearLayout layout_mobile=(LinearLayout) findViewById(R.id.linearlayout_bar_mobile);
-		final LinearLayout laout_mobile_pie=(LinearLayout) findViewById(R.id.linearlayout_piebar_mobile);
-		ismobileshowpie=false;
+		// 获取默认流量数值
+		final String PREFS_NAME = "allprefs";
+		final String VALUE_MOBILE_SET = "mobilemonthuse";
+		SharedPreferences prefs = context.getSharedPreferences(PREFS_NAME, 0);
+		long mobileSet = prefs.getLong(VALUE_MOBILE_SET, 50000000);
+		long moblileTotle = mobileTraffic[0] + mobileTraffic[63];
+		int usePercent = (int) (moblileTotle / mobileSet * 360);
+		if (usePercent > 360)
+			usePercent = 360;
+		int[] percent = new int[] { usePercent, 360 - usePercent };
+		final PieView pieView_mobile = new PieView(context, percent);
+		// View PieView=findViewById(R.id.pie_bar_mobile);
+		LinearLayout layout_mobile = (LinearLayout) findViewById(R.id.linearlayout_bar_mobile);
+		final LinearLayout laout_mobile_pie = (LinearLayout) findViewById(R.id.linearlayout_piebar_mobile);
+		ismobileshowpie = false;
 		layout_mobile.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
 				if (ismobileshowpie) {
 					laout_mobile_pie.removeAllViews();
-					ismobileshowpie=false;
-				}else{
+					ismobileshowpie = false;
+				} else {
 					laout_mobile_pie.removeAllViews();
 					laout_mobile_pie.addView(pieView_mobile);
-					ismobileshowpie=true;
+					ismobileshowpie = true;
 				}
 			}
 		});
-//		laout_mobile_pie.removeAllViews();
-//		laout_mobile_pie.addView(pieView_mobile);
-//		laout_mobile.removeAllViews();
+		// laout_mobile_pie.removeAllViews();
+		// laout_mobile_pie.addView(pieView_mobile);
+		// laout_mobile.removeAllViews();
 	}
 
 	/**
@@ -284,6 +366,55 @@ public class Main extends Activity {
 		RefreshProgressBar(mobile, wifi);
 	}
 
+	/**
+	 * 计算单月有几天
+	 * 
+	 * @param year
+	 *            输入年份
+	 * @param month
+	 *            输入月份
+	 * @return 返回天数
+	 */
+	private int countDay(int year, int month) {
+		if ((year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0))
+				&& month == 2) {
+			return 29;
+		} else {
+			switch (month) {
+			case 1:
+				return 31;
+			case 2:
+				return 28;
+			case 3:
+				return 31;
+			case 4:
+				return 30;
+			case 5:
+				return 31;
+			case 6:
+				return 30;
+			case 7:
+				return 31;
+			case 8:
+				return 31;
+			case 9:
+				return 30;
+			case 10:
+				return 31;
+			case 11:
+				return 30;
+			case 12:
+				return 31;
+			}
+		}
+		return 31;
+	}
+
+	/**
+	 * 显示日志
+	 * 
+	 * @param string
+	 */
 	private void showlog(String string) {
 		Log.d("main", string);
 	}
