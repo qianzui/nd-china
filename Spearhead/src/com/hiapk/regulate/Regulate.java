2e9f15f7ea091380ce8657fd24ae01678346d634
hqxmail <hqxmail@gmail.com>
2012-05-04 06:34:58 +0000

    新增查询流量设置页面

---
+++ Spearhead/src/com/hiapk/regulate/Regulate.java
@@ -0,0 +1,20 @@
+package com.hiapk.regulate;
+
+import com.hiapk.spearhead.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class Regulate extends Activity{
+	Button chooseBtn;
+	Button smsSend;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.regulate);
+		
+	}
+	
+}
