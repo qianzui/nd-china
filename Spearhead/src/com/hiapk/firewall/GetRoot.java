66dc42dc9cf88ea8d1ebd2c35013f58edecbce5a
fuweiwei0001 <fuweiwei0001@gmail.com>
2012-05-14 03:29:11 +0000

    查询是否包含root权限，并且复制防火墙文件。

--- Spearhead/src/com/hiapk/firewall/GetRoot.java
+++ Spearhead/src/com/hiapk/firewall/GetRoot.java
@@ -4,138 +4,156 @@ import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
+import com.hiapk.spearhead.R;
+
 import android.app.AlertDialog;
 import android.content.Context;
+import android.widget.Toast;
 
 public class GetRoot {
 
-   private static boolean hasRoot = false;
-   private static final String SCRIPT_FILE = "firewall.sh";
-	//获取root权限
-	public static boolean cmdRoot(String cmd)
-	{
+	private static boolean hasRoot = false;
+	private static final String SCRIPT_FILE = "firewall.sh";
+
+	// 获取root权限
+	public static boolean cmdRoot(String cmd) {
 		Process process = null;
-		        DataOutputStream os = null;
-		        try{
-		            process = Runtime.getRuntime().exec("su");
-		            os = new DataOutputStream(process.getOutputStream());
-		            os.writeBytes(cmd+ "\n");
-		            os.writeBytes("exit\n");
-		            os.flush();
-		            process.waitFor();
-		        } catch (Exception e) {
-		            return false;
-		        } finally {
-		            try {
-		                if (os != null)   {
-		                    os.close();
-		                }
-		                process.destroy();
-		            } catch (Exception e) {
-		            }
-		        }
-		        return true;
-		    }
-	//是否root过
+		DataOutputStream os = null;
+		try {
+			process = Runtime.getRuntime().exec("su");
+			os = new DataOutputStream(process.getOutputStream());
+			os.writeBytes(cmd + "\n");
+			os.writeBytes("exit\n");
+			os.flush();
+			process.waitFor();
+		} catch (Exception e) {
+			return false;
+		} finally {
+			try {
+				if (os != null) {
+					os.close();
+				}
+				process.destroy();
+			} catch (Exception e) {
+			}
+		}
+		return true;
+	}
+
+	// 是否root过
 	public static boolean isRoot() {
 		boolean blnResult = false;
 		File su = new File("/tmp/su.txt");
 		FileOutputStream fos = null;
 		try {
-		     fos = new FileOutputStream(su);
+			fos = new FileOutputStream(su);
 		} catch (Exception e) {
-		     e.printStackTrace();
-		   } finally {
-	     if (fos != null) {
-	 	    try {
-		          fos.close();
-		         } catch (Exception e) {
-		        e.printStackTrace();
-	              	}
-	              }
-	          	}
-		if (su.exists()) {
-		   blnResult = true;
-		   try {
-		     su.delete();
-		       } catch (SecurityException e) {
-	             	e.printStackTrace();
-	           	}
-	    	}
-		return blnResult;
+			e.printStackTrace();
+		} finally {
+			if (fos != null) {
+				try {
+					fos.close();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
 		}
-		
-     public static boolean hasRootAccess(Context context , boolean showErrors)
-     {
-    	if(hasRoot)
-    	   return true;
-    	final StringBuilder res = new StringBuilder();
-    	try{
-    		if(runScriptAsRoot(context,"exit 0",res) == 0)
-    		{
-    			hasRoot = true ;
-    			return true;
-    		}
-    	}catch(Exception e){
-    		}
-    	if(showErrors)
-    	{
-    		new AlertDialog.Builder(context)
-			.setNeutralButton("确定", null)
-			.setMessage("Could not acquire root access.\n"
-					+ "You need a rooted phone to run DroidWall.\n\n"
-					+ "If this phone is already rooted, please make sure DroidWall has enough permissions to execute the \"su\" command.\n"
-					+ "Error message: " + res.toString()).show();
-    	}
-    	return false;
-     }
-     
-     public static int runScriptAsRoot(Context context ,String script , StringBuilder res) throws IOException
-     {
-		return runScriptAsRoot(context,script ,res,40000);
-     }
-     public static int runScriptAsRoot(Context context,String script , StringBuilder res,long timeout)
-     {
-		return runScript(context,script,res,timeout,true);
-     }
-     public static int runScript(Context context,String script ,StringBuilder res,long timeout, boolean asroot)
-     {
-    	 final File file = new File(context.getDir("bin", 0),SCRIPT_FILE);
-    	 final ScriptRunner runner =  new ScriptRunner(file ,script ,res,asroot);
-    	 runner.start();
-    	 try{
-    		 if(timeout > 0){
-    			 runner.join(timeout);
-    		 }else{
-    			 runner.join();
-    		 }
-    		 if(runner.isAlive())
-    		 {
-    			 runner.interrupt();
-    			 runner.join(150);
-    			 runner.destroy();
-    			 runner.join(50);
-    		 }
-    	 }catch(Exception e){
-    		 
-    	 }
+		if (su.exists()) {
+			blnResult = true;
+			try {
+				su.delete();
+			} catch (SecurityException e) {
+				e.printStackTrace();
+			}
+		}
+		return blnResult;
+	}
+
+	/**
+	 * 查看是否有root权限
+	 * 
+	 * @param context
+	 * @param showErrors
+	 *            是否显示错误信息
+	 * @return 返回是否具有root权限
+	 */
+	public boolean hasRootAccess(Context context, boolean showErrors) {
+		if (hasRoot)
+			return true;
+		final StringBuilder res = new StringBuilder();
+		try {
+			if (runScript(context, "exit 0", res, 20000, true) == 0) {
+				hasRoot = true;
+				assertBinaries(context, true);
+				return true;
+			}
+		} catch (Exception e) {
+		}
+		if (showErrors) {
+			new AlertDialog.Builder(context).setNeutralButton("确定", null)
+					.setMessage("无法获取root权限.\n"
+					// + "You need a rooted phone to run DroidWall.\n\n"
+					// +
+					// "If this phone is already rooted, please make sure DroidWall has enough permissions to execute the \"su\" command.\n"
+							+ "错误信息: " + res.toString()).show();
+		}
+		return false;
+	}
+
+	/**
+	 * Runs a script, wither as root or as a regular user (multiple commands
+	 * separated by "\n").
+	 * 
+	 * @param ctx
+	 *            mandatory context
+	 * @param script
+	 *            the script to be executed
+	 * @param res
+	 *            the script output response (stdout + stderr)
+	 * @param timeout
+	 *            timeout in milliseconds (-1 for none)
+	 * @return the script exit code
+	 */
+	public static int runScript(Context context, String script,
+			StringBuilder res, long timeout, boolean asroot) {
+		final File file = new File(context.getDir("bin", 0), SCRIPT_FILE);
+		final ScriptRunner runner = new ScriptRunner(file, script, res, asroot);
+		runner.start();
+		try {
+			if (timeout > 0) {
+				runner.join(timeout);
+			} else {
+				runner.join();
+			}
+			if (runner.isAlive()) {
+				runner.interrupt();
+				runner.join(150);
+				runner.destroy();
+				runner.join(50);
+			}
+		} catch (Exception e) {
+
+		}
 		return runner.exitcode;
-    	 
-     }
-     
-     
-     private static final class ScriptRunner extends Thread{
-    	 private final File file ;
-    	 private final String script;
-    	 private final StringBuilder res;
- 		 private final boolean asroot;
- 		 public int exitcode = -1;
- 		 private Process exec;
- 		 
- 	     public ScriptRunner(File file, String script, StringBuilder res,
+
+	}
+
+	/**
+	 * Internal thread used to execute scripts (as root or not).
+	 */
+	private static final class ScriptRunner extends Thread {
+		private final File file;
+		private final String script;
+		private final StringBuilder res;
+		private final boolean asroot;
+		public int exitcode = -1;
+		private Process exec;
+
+		public ScriptRunner(File file, String script, StringBuilder res,
 				boolean asroot) {
 			this.file = file;
 			this.script = script;
@@ -198,6 +216,7 @@ public class GetRoot {
 				destroy();
 			}
 		}
+
 		/**
 		 * Destroy this script runner
 		 */
@@ -206,7 +225,89 @@ public class GetRoot {
 				exec.destroy();
 			exec = null;
 		}
-     }
-     
+	}
 
+	/**
+	 * Asserts that the binary files are installed in the cache directory.
+	 * 
+	 * @param ctx
+	 *            context
+	 * @param showErrors
+	 *            indicates if errors should be alerted
+	 * @return false if the binary files could not be installed
+	 */
+	private static boolean assertBinaries(Context ctx, boolean showErrors) {
+		boolean changed = false;
+		try {
+			// Check iptables_armv5
+			File file = new File(ctx.getDir("bin", 0), "iptables_armv5");
+			if (!file.exists()) {
+				copyRawFile(ctx, R.raw.busybox_g1, file, "755");
+				changed = true;
+			}
+			// Check busybox
+			file = new File(ctx.getDir("bin", 0), "busybox_g1");
+			if (!file.exists()) {
+				copyRawFile(ctx, R.raw.busybox_g1, file, "755");
+				changed = true;
+			}
+			if (changed) {
+				Toast.makeText(ctx, "anzhuang", Toast.LENGTH_LONG).show();
+			}
+		} catch (Exception e) {
+			if (showErrors)
+				alert(ctx, "安装防火墙文件失败: " + e);
+			return false;
+		}
+		return true;
+	}
+
+	/**
+	 * Copies a raw resource file, given its ID to the given location
+	 * 
+	 * @param ctx
+	 *            context
+	 * @param resid
+	 *            resource id
+	 * @param file
+	 *            destination file
+	 * @param mode
+	 *            file permissions (E.g.: "755")
+	 * @throws IOException
+	 *             on error
+	 * @throws InterruptedException
+	 *             when interrupted
+	 */
+	private static void copyRawFile(Context ctx, int resid, File file,
+			String mode) throws IOException, InterruptedException {
+		final String abspath = file.getAbsolutePath();
+		// Write the iptables binary
+		final FileOutputStream out = new FileOutputStream(file);
+		final InputStream is = ctx.getResources().openRawResource(resid);
+		byte buf[] = new byte[1024];
+		int len;
+		while ((len = is.read(buf)) > 0) {
+			out.write(buf, 0, len);
+		}
+		out.close();
+		is.close();
+		// Change the permissions
+		Runtime.getRuntime().exec("chmod " + mode + " " + abspath).waitFor();
+	}
+
+	/**
+	 * Display a simple alert box
+	 * 
+	 * @param ctx
+	 *            context
+	 * @param msg
+	 *            message
+	 */
+	public static void alert(Context ctx, CharSequence msg) {
+		if (ctx != null) {
+			new AlertDialog.Builder(ctx)
+					.setNeutralButton(android.R.string.ok, null)
+					.setMessage(msg).show();
+		}
+	}
 }
