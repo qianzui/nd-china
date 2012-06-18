ce1830cbeebbd9e62866f6aa48dd1766727637a7
fuweiwei0001 <fuweiwei0001@gmail.com>
2012-06-18 07:28:14 +0000

    添加按钮及对话框

---
+++ Spearhead/src/com/hiapk/alertdialog/CustomDialogMain3Been.java
@@ -0,0 +1,343 @@
+package com.hiapk.alertdialog;
+
+import java.text.DecimalFormat;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences.Editor;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import com.hiapk.dataexe.TrafficManager;
+import com.hiapk.dataexe.UnitHandler;
+import com.hiapk.prefrencesetting.PrefrenceOperatorUnit;
+import com.hiapk.prefrencesetting.SharedPrefrenceData;
+import com.hiapk.regulate.PhoneSet;
+import com.hiapk.regulate.Regulate;
+import com.hiapk.spearhead.Main3;
+import com.hiapk.spearhead.R;
+import com.hiapk.widget.SetText;
+
+public class CustomDialogMain3Been {
+	// 操作sharedprefrence
+	String PREFS_NAME = "allprefs";
+	// 总流量long
+	String VALUE_MOBILE_SET = "mobilemonthuse";
+	// 显示在预警页面的int
+	String VALUE_MOBILE_SET_OF_INT = "mobilemonthuseinint";
+	// 设置单位（月度设置）
+	String MOBILE_SET_UNIT = "mobileMonthUnit";
+	// 设置结算日期及结算日期的设施时间，日期等
+	String MOBILE_COUNT_DAY = "mobileMonthCountDay";
+	String MOBILE_COUNT_SET_YEAR = "mobileMonthSetCountYear";
+	String MOBILE_COUNT_SET_MONTH = "mobileMonthSetCountMonth";
+	String MOBILE_COUNT_SET_DAY = "mobileMonthSetCountDay";
+	String MOBILE_COUNT_SET_TIME = "mobileMonthSetCountTime";
+	// 已使用总流量int
+	String VALUE_MOBILE_HASUSED_OF_FLOAT = "mobileHasusedint";
+	// 设置单位（已使用）
+	String MOBILE_HASUSED_SET_UNIT = "mobileHasusedUnit";
+	// 已使用总流量long
+	String VALUE_MOBILE_HASUSED_LONG = "mobileHasusedlong";
+	// 流量预警
+	String MOBILE_WARNING_MONTH = "mobilemonthwarning";
+	String MOBILE_WARNING_DAY = "mobiledaywarning";
+	// 预警动作
+	String WARNING_ACTION = "warningaction";
+	Context context;
+
+	public CustomDialogMain3Been(Context context) {
+		this.context = context;
+	}
+
+	/**
+	 * 本月已用弹出的对话框
+	 * 
+	 * @param TextView_month
+	 *            传入点击的TextView
+	 * @return 返回对话框
+	 */
+	public void dialogMonthHasUsed() {
+		// TODO Auto-generated method stub
+		final SharedPrefrenceData sharedData = new SharedPrefrenceData(context);
+		final DecimalFormat format = new DecimalFormat("0.##");
+		int mobileUseUnit = sharedData.getMonthHasUsedUnit();
+		// float mobileUsefloat = sharedData.getMonthMobileHasUseOffloat();
+		long mobileUselong = TrafficManager.getMonthUseData(context);
+		// 初始化窗体
+		LayoutInflater factory = LayoutInflater.from(context);
+		final View textEntryView = factory.inflate(
+				R.layout.custom_dialog_on_main_text_entry, null);
+		final EditText et_month = (EditText) textEntryView
+				.findViewById(R.id.ev_alert);
+		final Spinner spin_unit = (Spinner) textEntryView
+				.findViewById(R.id.sp_unit);
+		ArrayAdapter<CharSequence> adp = ArrayAdapter.createFromResource(
+				context, R.array.unit, R.layout.sptext_on_alert);
+		adp.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		spin_unit.setAdapter(adp);
+		// 初始化数值
+		spin_unit.setSelection(mobileUseUnit);
+		int mobileHasUsedUnit = spin_unit.getSelectedItemPosition();
+		// 若数值为0，则显示空
+		if (mobileUselong != 0) {
+			String mobileUseString;
+			if (mobileHasUsedUnit == 0) {
+				mobileUseString = format
+						.format(((double) mobileUselong) / 1024 / 1024);
+			} else {
+				mobileUseString = format
+						.format(((double) mobileUselong) / 1024 / 1024 / 1024);
+			}
+
+			et_month.setText(mobileUseString);
+			et_month.setSelection(String.valueOf(mobileUseString).length());
+		} else {
+			et_month.setText("");
+		}
+
+		final CustomDialog monthHasUsedAlert = new CustomDialog.Builder(context)
+				.setTitle("请设置本月已用流量").setContentView(textEntryView)
+				.setOtherButton("短信查询", null).setPositiveButton("确定", null)
+				.setNegativeButton("取消", null).create();
+		monthHasUsedAlert.show();
+
+		Button btn_other = (Button) monthHasUsedAlert
+				.findViewById(R.id.otherButton);
+		btn_other.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				monthHasUsedAlert.dismiss();
+				Intent intent = new Intent(context, Regulate.class);
+				context.startActivity(intent);
+
+			}
+		});
+		Button btn_ok = (Button) monthHasUsedAlert
+				.findViewById(R.id.positiveButton);
+		btn_ok.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				// 输入的数值
+				float i = 0;
+				try {
+					i = Float.valueOf(et_month.getText().toString());
+				} catch (NumberFormatException e) {
+					// TODO: handle exception
+					showlog(i + "shuziError" + et_month.getText().toString());
+				}
+
+				// btn_Used.setText(format.format(i));
+				int mobileHasUsedUnit = spin_unit.getSelectedItemPosition();
+				Editor passfileEditor = context.getSharedPreferences(
+						PREFS_NAME, 0).edit();
+				// Log.d("main3", i + "");
+				//
+				//
+
+				if (mobileHasUsedUnit == 0) {
+					passfileEditor.putLong(VALUE_MOBILE_HASUSED_LONG,
+							(long) (i * 1048576));
+				} else {
+					passfileEditor.putLong(VALUE_MOBILE_HASUSED_LONG,
+							(long) (i * 1048576 * 1024));
+				}
+				passfileEditor.putFloat(VALUE_MOBILE_HASUSED_OF_FLOAT, i);
+
+				passfileEditor.putInt(MOBILE_HASUSED_SET_UNIT,
+						mobileHasUsedUnit);
+				passfileEditor.commit();// 委托，存入数据
+				// commitUsedTrafficTime();
+				// init_btn_HasUsed();
+				/* User clicked OK so do some stuff */
+				long hasusedlong = sharedData.getMonthMobileHasUse();
+				long setlong = sharedData.getMonthMobileSetOfLong();
+				if (hasusedlong > setlong) {
+					dialogHasUsedLongTooMuch();
+				}
+				SetText.resetWidgetAndNotify(context);
+				monthHasUsedAlert.dismiss();
+
+			}
+		});
+		Button btn_cancel = (Button) monthHasUsedAlert
+				.findViewById(R.id.negativeButton);
+		btn_cancel.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				monthHasUsedAlert.dismiss();
+			}
+		});
+
+	}
+
+	/**
+	 * 设置的本月已用流量超过包月流量
+	 * 
+	 * @return
+	 */
+	public void dialogHasUsedLongTooMuch() {
+		final CustomDialog dayWarning = new CustomDialog.Builder(context)
+				.setTitle("注意！").setMessage("您设置的本月已用流量超过包月流量！")
+				// .setView(textEntryView)
+				.setPositiveButton("确定", null).create();
+		dayWarning.show();
+		Button btn_cancel = (Button) dayWarning
+				.findViewById(R.id.positiveButton);
+		btn_cancel.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				PrefrenceOperatorUnit.resetHasWarning(context);
+				dayWarning.dismiss();
+			}
+		});
+
+	}
+
+	/**
+	 * 月度显示弹出的对话框
+	 * 
+	 * @param TextView_month
+	 *            传入点击的TextView
+	 * @return 返回对话框
+	 */
+	public void dialogMonthSet_Main3(final Button btn_month,
+			final Button dayWarning, final Button monthWarning) {
+
+		SharedPrefrenceData sharedData = new SharedPrefrenceData(context);
+		int mobileUnit = sharedData.getMonthMobileSetUnit();
+		int mobileSetInt = sharedData.getMonthMobileSetOfint();
+		// 初始化窗体
+		LayoutInflater factory = LayoutInflater.from(context);
+		final View textEntryView = factory.inflate(
+				R.layout.custom_dialog_on_main_text_entry, null);
+		final EditText et_month = (EditText) textEntryView
+				.findViewById(R.id.ev_alert);
+		final Spinner spin_unit = (Spinner) textEntryView
+				.findViewById(R.id.sp_unit);
+		ArrayAdapter<CharSequence> adp = ArrayAdapter.createFromResource(
+				context, R.array.unit, R.layout.sptext_on_alert);
+		adp.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		spin_unit.setAdapter(adp);
+		// 初始化数值
+		spin_unit.setSelection(mobileUnit);
+		// 判断0
+		if (mobileSetInt != 0) {
+			et_month.setText(mobileSetInt + "");
+		} else {
+			et_month.setText("");
+		}
+		et_month.setSelection(String.valueOf(mobileSetInt).length());
+		final CustomDialog monthSetAlert = new CustomDialog.Builder(context)
+				.setTitle("请设置每月流量限额").setContentView(textEntryView)
+				.setPositiveButton("确定", null).setNegativeButton("取消", null)
+				.create();
+		monthSetAlert.show();
+
+		Button btn_ok = (Button) monthSetAlert
+				.findViewById(R.id.positiveButton);
+		btn_ok.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				// 输入的数值
+				int i = 0;
+				try {
+					i = Integer.valueOf(et_month.getText().toString());
+				} catch (NumberFormatException e) {
+					// TODO: handle exception
+					i = 0;
+				}
+				// showlog(i + "");
+				int mobileUnit = spin_unit.getSelectedItemPosition();
+				Editor passfileEditor = context.getSharedPreferences(
+						PREFS_NAME, 0).edit();
+				// Log.d("main3", i + "");
+
+				if (mobileUnit == 0) {
+					long monthsetTraffMB = (long) i * 1024 * 1024;
+					// showlog(monthsetTraffMB + "");
+					passfileEditor.putLong(VALUE_MOBILE_SET, monthsetTraffMB);
+					passfileEditor.putLong(MOBILE_WARNING_MONTH,
+							monthsetTraffMB * 9 / 10);
+					passfileEditor.putLong(MOBILE_WARNING_DAY,
+							monthsetTraffMB / 10);
+					// 设置会改变的3个数值。++
+					UnitHandler FormatUnit = new UnitHandler();
+					btn_month.setText(FormatUnit.unitHandler(monthsetTraffMB));
+					dayWarning.setText(FormatUnit
+							.unitHandler(monthsetTraffMB * 9 / 10));
+					monthWarning.setText(FormatUnit
+							.unitHandler(monthsetTraffMB / 10));
+				} else if (mobileUnit == 1) {
+					long monthsetTraffGB = (long) i * 1024 * 1024 * 1024;
+					// showlog(monthsetTraffGB + "");
+					passfileEditor.putLong(VALUE_MOBILE_SET, monthsetTraffGB);
+					passfileEditor.putLong(MOBILE_WARNING_MONTH,
+							monthsetTraffGB * 9 / 10);
+					passfileEditor.putLong(MOBILE_WARNING_DAY,
+							monthsetTraffGB / 10);
+					// 设置会改变的3个数值。++
+					UnitHandler FormatUnit = new UnitHandler();
+					btn_month.setText(FormatUnit.unitHandler(monthsetTraffGB));
+					dayWarning.setText(FormatUnit
+							.unitHandler(monthsetTraffGB * 9 / 10));
+					monthWarning.setText(FormatUnit
+							.unitHandler(monthsetTraffGB / 10));
+				}
+				passfileEditor.putInt(MOBILE_SET_UNIT, mobileUnit);
+				passfileEditor.putInt(VALUE_MOBILE_SET_OF_INT, i);
+				passfileEditor.commit();// 委托，存入数据
+				SetText.resetWidgetAndNotify(context);
+				// showlog(mobileSetLong + "");
+				SharedPrefrenceData sharedData = new SharedPrefrenceData(
+						context);
+				// long monthSetofLong = sharedData.getMonthMobileSetOfLong();
+				if (i == 0) {
+					sharedData.setMonthSetHasSet(false);
+				} else {
+					sharedData.setMonthSetHasSet(true);
+				}
+				monthSetAlert.dismiss();
+				/* User clicked OK so do some stuff */
+				// MobclickAgent.onEvent(context, "monthUse",
+				// String.valueOf(i));
+			}
+		});
+
+		Button btn_cancel = (Button) monthSetAlert
+				.findViewById(R.id.negativeButton);
+		btn_cancel.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				monthSetAlert.dismiss();
+			}
+		});
+	}
+
+	/**
+	 * 显示日志
+	 * 
+	 * @param string
+	 */
+	private void showlog(String string) {
+		// Log.d("main3", string);
+	}
+}
