243c1b76eb7fc90c32c65ec641616f1a72983963
fuweiwei0001@gmail.com <fuweiwei0001@gmail.com@a794dee8-fd84-c021-6015-35cb739101f1>
2012-06-27 02:43:00 +0000

    修改防火墙单位。

--- trunk/Spearhead/src/com/hiapk/spearhead/FireWallActivity.java
+++ trunk/Spearhead/src/com/hiapk/spearhead/FireWallActivity.java
@@ -6,6 +6,7 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import com.hiapk.broadcreceiver.AlarmSet;
+import com.hiapk.dataexe.UnitHandler;
 import com.hiapk.firewall.AppInfo;
 import com.hiapk.firewall.AppListAdapter;
 import com.hiapk.firewall.Block;
@@ -99,8 +100,8 @@ public class FireWallActivity extends Activity {
 	}
 
 	public void initList() {
-//		pro = ProgressDialog.show(mContext, "提示",
-//				"获取列表中,请耐心等待,获取的时间长短取决于您安装软件的数量...");
+		// pro = ProgressDialog.show(mContext, "提示",
+		// "获取列表中,请耐心等待,获取的时间长短取决于您安装软件的数量...");
 		CustomProgressDialog customProgressDialog = new CustomProgressDialog(
 				mContext);
 		customdialog = customProgressDialog.createDialog(mContext);
@@ -113,15 +114,16 @@ public class FireWallActivity extends Activity {
 				try {
 					setAdapter();
 					customdialog.dismiss();
-		 			if (Block.isShowHelp(mContext)) {
+					if (Block.isShowHelp(mContext)) {
 						SpearheadActivity.showHelp(mContext);
 						Block.isShowHelpSet(mContext, false);
-					}else{
-					if (Block.fireTip(mContext)) {
-						Toast.makeText(mContext, "下拉列表可以进行刷新!", Toast.LENGTH_SHORT).show();
+					} else {
+						if (Block.fireTip(mContext)) {
+							Toast.makeText(mContext, "下拉列表可以进行刷新!",
+									Toast.LENGTH_SHORT).show();
+						}
 					}
-					}
-//					pro.dismiss();
+					// pro.dismiss();
 				} catch (Exception ex) {
 				}
 			}
@@ -138,7 +140,7 @@ public class FireWallActivity extends Activity {
 
 	public void setAdapter() {
 		appListAdapter = new AppListAdapter(FireWallActivity.this, mList);
-		appListView = (MyListView)findViewById(R.id.app_list);
+		appListView = (MyListView) findViewById(R.id.app_list);
 		appListView.setAdapter(appListAdapter);
 		appListView.setOnItemClickListener(new OnItemClickListener() {
 			@Override
@@ -154,6 +156,7 @@ public class FireWallActivity extends Activity {
 					protected Void doInBackground(Void... params) {
 						return null;
 					}
+
 					@Override
 					protected void onPostExecute(Void result) {
 						mList = comp(getList(mContext));
@@ -164,25 +167,25 @@ public class FireWallActivity extends Activity {
 				}.execute();
 			}
 		});
-	}  
+	}
 
 	public ArrayList<AppInfo> getList(Context context) {
 		packageInfo = context.getPackageManager().getInstalledPackages(0);
 		PackageManager pm = getPackageManager();
 		ArrayList<AppInfo> appList = new ArrayList<AppInfo>();
-		
-		Log.i("start..........",System.currentTimeMillis() + "");
-		do{
-		   AlarmSet alset = new AlarmSet();
-		   alset.StartAlarmUidTotal(mContext);
-		   if(SQLStatic.uiddata != null){
-		      mp = SQLStatic.uiddata;
-		      break;  
-		       } 
-	     	} while(mp == null);    
-		Log.i("end..........",System.currentTimeMillis() + "");
+
+		Log.i("start..........", System.currentTimeMillis() + "");
+		do {
+			AlarmSet alset = new AlarmSet();
+			alset.StartAlarmUidTotal(mContext);
+			if (SQLStatic.uiddata != null) {
+				mp = SQLStatic.uiddata;
+				break;
+			}
+		} while (mp == null);
+		Log.i("end..........", System.currentTimeMillis() + "");
 		for (int i = 0; i < packageInfo.size(); i++) {
-			PackageInfo pkgInfo = packageInfo.get(i);  
+			PackageInfo pkgInfo = packageInfo.get(i);
 			int uid = pkgInfo.applicationInfo.uid;
 			if (pkgInfo.applicationInfo.uid >= 10000
 					&& PackageManager.PERMISSION_GRANTED == pm.checkPermission(
@@ -199,33 +202,34 @@ public class FireWallActivity extends Activity {
 						up = -1000;
 						down = -1000;
 					}
-					AppInfo ai  = new AppInfo(pkgInfo.applicationInfo.loadIcon(pm)
-							, pkgInfo.applicationInfo.loadLabel(pm).toString(), 
-							pkgInfo.applicationInfo.packageName, 
-							uid, up, down);
+					AppInfo ai = new AppInfo(
+							pkgInfo.applicationInfo.loadIcon(pm),
+							pkgInfo.applicationInfo.loadLabel(pm).toString(),
+							pkgInfo.applicationInfo.packageName, uid, up, down);
 					appList.add(ai);
 				}
 			}
 		}
 		return appList;
 	}
-	public  ArrayList<AppInfo> comp(ArrayList<AppInfo> appList){
+
+	public ArrayList<AppInfo> comp(ArrayList<AppInfo> appList) {
 		ArrayList<AppInfo> showList = new ArrayList<AppInfo>();
 		ArrayList<AppInfo> showList2 = new ArrayList<AppInfo>();
 		for (int i = 0; i < appList.size(); i++) {
 			AppInfo ai1 = appList.get(i);
 			for (int j = i; j < appList.size(); j++) {
 				AppInfo ai2 = appList.get(j);
-				if(ai1.up + ai1.down < ai2.up + ai2.down){
-					AppInfo temp = new AppInfo(ai1.d, ai1.appname, ai1.packagename,
-							ai1.uid, ai1.up, ai1.down);
+				if (ai1.up + ai1.down < ai2.up + ai2.down) {
+					AppInfo temp = new AppInfo(ai1.d, ai1.appname,
+							ai1.packagename, ai1.uid, ai1.up, ai1.down);
 					ai1.appname = ai2.appname;
 					ai1.d = ai2.d;
 					ai1.packagename = ai2.packagename;
 					ai1.uid = ai2.uid;
 					ai1.up = ai2.up;
 					ai1.down = ai2.down;
-					
+
 					ai2.appname = temp.appname;
 					ai2.d = temp.d;
 					ai2.packagename = temp.packagename;
@@ -237,36 +241,23 @@ public class FireWallActivity extends Activity {
 		}
 		for (int i = 0; i < appList.size(); i++) {
 			AppInfo ai = appList.get(i);
-			if(ai.up + ai.down > 0){
+			if (ai.up + ai.down > 0) {
 				showList.add(ai);
-			}else{
+			} else {
 				showList2.add(ai);
 			}
 		}
-        MyComparator mc = new MyComparator();
-        Collections.sort(showList2,mc);
+		MyComparator mc = new MyComparator();
+		Collections.sort(showList2, mc);
 		for (int i = 0; i < showList2.size(); i++) {
 			AppInfo ai = showList2.get(i);
 			showList.add(ai);
 		}
 		return showList;
 	}
-	
-	public String unitHandler(long count) {
-		String value = null;
-		long temp = count;
-		float floatnum = count;
-		if ((temp = temp / 1024) < 1) {
-			value = count + "B";
-		} else if ((floatnum = (float) temp / 1024) < 1) {
-			value = temp + "KB";
-		} else {
-			DecimalFormat format = new DecimalFormat("0.##");
-			value = format.format(floatnum) + "MB";
-		}
-		return value;
-	}
+
 	public void menuDialog(View arg1) {
+		UnitHandler unithandler = new UnitHandler();
 		final AppInfo pkgInfo = (AppInfo) arg1.getTag(R.id.tag_pkginfo);
 		final Drawable icon = pkgInfo.d;
 		final int uid = pkgInfo.uid;
@@ -284,79 +275,90 @@ public class FireWallActivity extends Activity {
 		final String trafficup;
 		final String trafficdown;
 		if (up == -1000 && down == -1000) {
-			trafficup = unitHandler(0);
-			trafficdown = unitHandler(0);
+			trafficup = unithandler.unitHandlerAccurate(0);
+			trafficdown = unithandler.unitHandlerAccurate(0);
 		} else {
-			trafficup = unitHandler(up);
-			trafficdown = unitHandler(down);
+			trafficup = unithandler.unitHandlerAccurate(up);
+			trafficdown = unithandler.unitHandlerAccurate(down);
 		}
-		final Drawable d = mContext.getResources().getDrawable(R.drawable.bg_fire_option);
-	    LayoutInflater factory = LayoutInflater.from(mContext);
+		final Drawable d = mContext.getResources().getDrawable(
+				R.drawable.bg_fire_option);
+		LayoutInflater factory = LayoutInflater.from(mContext);
 		final View mDialogView = factory.inflate(R.layout.fire_options, null);
-		final AlertDialog mDialog = new AlertDialog.Builder(FireWallActivity.this).create();
+		final AlertDialog mDialog = new AlertDialog.Builder(
+				FireWallActivity.this).create();
 		mDialog.show();
 		Window window = mDialog.getWindow();
 		window.setContentView(mDialogView, new LayoutParams(
 				LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
 		final int heigh = window.getWindowManager().getDefaultDisplay()
-		.getHeight();
-        final int width = window.getWindowManager().getDefaultDisplay()
-		.getWidth();
-    	window.setLayout((int) (width * 0.8),LayoutParams.WRAP_CONTENT);
-		final TextView manager = (TextView)mDialogView.findViewById(R.id.button_manager);
-		final TextView detail = (TextView)mDialogView.findViewById(R.id.button_detail);
-		final TextView uninstalled = (TextView)mDialogView.findViewById(R.id.button_uninstall);
-		final TextView back = (TextView)mDialogView.findViewById(R.id.button_back);
-		
-		manager.setOnClickListener(new OnClickListener(){
+				.getHeight();
+		final int width = window.getWindowManager().getDefaultDisplay()
+				.getWidth();
+		window.setLayout((int) (width * 0.8), LayoutParams.WRAP_CONTENT);
+		final TextView manager = (TextView) mDialogView
+				.findViewById(R.id.button_manager);
+		final TextView detail = (TextView) mDialogView
+				.findViewById(R.id.button_detail);
+		final TextView uninstalled = (TextView) mDialogView
+				.findViewById(R.id.button_uninstall);
+		final TextView back = (TextView) mDialogView
+				.findViewById(R.id.button_back);
+
+		manager.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				showInstalledAppDetails(FireWallActivity.this,
-						pkname);
+				showInstalledAppDetails(FireWallActivity.this, pkname);
 				manager.setBackgroundDrawable(d);
 				mDialog.cancel();
-				
+
 			}
 		});
-		
-		detail.setOnClickListener(new OnClickListener(){
+
+		detail.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-			    LayoutInflater infalter = LayoutInflater.from(mContext);
-			    final View  mDetailView = infalter.inflate(R.layout.fire_detail,null);
-				final AlertDialog detailDialog = new AlertDialog.Builder(FireWallActivity.this).create();
+				LayoutInflater infalter = LayoutInflater.from(mContext);
+				final View mDetailView = infalter.inflate(R.layout.fire_detail,
+						null);
+				final AlertDialog detailDialog = new AlertDialog.Builder(
+						FireWallActivity.this).create();
 				detailDialog.show();
 				Window wd = detailDialog.getWindow();
 				wd.setContentView(mDetailView, new LayoutParams(
-				LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
-				wd.setLayout((int) (width * 0.8),LayoutParams.WRAP_CONTENT);
-				
-				final TextView traffic_up = (TextView)mDetailView.findViewById(R.id.fire_traffic_up);
-				final TextView traffic_down = (TextView)mDetailView.findViewById(R.id.fire_traffic_down);
-				final Button detail_ok = (Button)mDetailView.findViewById(R.id.detail_ok);
-				final Button detail_history = (Button)mDetailView.findViewById(R.id.detail_history);
-				
-				traffic_up.setText("上传： "+ trafficup);
-				traffic_down.setText("下载： "+ trafficdown);
-				
-				detail_ok.setOnClickListener(new Button.OnClickListener(){
+						LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
+				wd.setLayout((int) (width * 0.8), LayoutParams.WRAP_CONTENT);
+
+				final TextView traffic_up = (TextView) mDetailView
+						.findViewById(R.id.fire_traffic_up);
+				final TextView traffic_down = (TextView) mDetailView
+						.findViewById(R.id.fire_traffic_down);
+				final Button detail_ok = (Button) mDetailView
+						.findViewById(R.id.detail_ok);
+				final Button detail_history = (Button) mDetailView
+						.findViewById(R.id.detail_history);
+
+				traffic_up.setText("上传： " + trafficup);
+				traffic_down.setText("下载： " + trafficdown);
+
+				detail_ok.setOnClickListener(new Button.OnClickListener() {
 					@Override
 					public void onClick(View v) {
 						// TODO Auto-generated method stub
 						detailDialog.cancel();
 					}
 				});
-				detail_history.setOnClickListener(new Button.OnClickListener(){
+				detail_history.setOnClickListener(new Button.OnClickListener() {
 					@Override
 					public void onClick(View v) {
 						// TODO Auto-generated method stub
 						Intent intent = new Intent();
-						intent.setClass(mContext,UidMonthTraff.class);
+						intent.setClass(mContext, UidMonthTraff.class);
 						Bundle bData = new Bundle();
 						bData.putInt("uid", uid);
-						bData.putString("appname",appname);
-						bData.putString("pkname",pkname);
+						bData.putString("appname", appname);
+						bData.putString("pkname", pkname);
 						intent.putExtras(bData);
 						mContext.startActivity(intent);
 						detailDialog.cancel();
@@ -364,25 +366,24 @@ public class FireWallActivity extends Activity {
 				});
 				detail.setBackgroundDrawable(d);
 				mDialog.cancel();
-			} 
+			}
 		});
-		
-		uninstalled.setOnClickListener(new OnClickListener(){
+
+		uninstalled.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
 				Uri uri = Uri.fromParts("package", pkname, null);
-				Intent intent = new Intent(Intent.ACTION_DELETE,
-						uri);
+				Intent intent = new Intent(Intent.ACTION_DELETE, uri);
 				startActivity(intent);
 				uninstalled.setBackgroundDrawable(d);
 				mDialog.cancel();
 			}
-			
+
 		});
-		
-		back.setOnClickListener(new OnClickListener(){
+
+		back.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
@@ -390,7 +391,7 @@ public class FireWallActivity extends Activity {
 				back.setBackgroundDrawable(d);
 				mDialog.cancel();
 			}
-			
+
 		});
 	}
 
