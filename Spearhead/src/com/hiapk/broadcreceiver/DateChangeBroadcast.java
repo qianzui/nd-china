2a4f20d5e1f71e3947ab85be27f17ea69a76d582
fuweiwei0001 <fuweiwei0001@gmail.com>
2012-07-04 02:39:36 +0000

    定期将月度流量清零

---
+++ Spearhead/src/com/hiapk/broadcreceiver/DateChangeBroadcast.java
@@ -0,0 +1,51 @@
+package com.hiapk.broadcreceiver;
+
+import com.hiapk.prefrencesetting.SharedPrefrenceData;
+import com.hiapk.widget.SetText;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.text.format.Time;
+import android.util.Log;
+
+public class DateChangeBroadcast extends BroadcastReceiver {
+	// private static final String ACTION_TIME_CHANGED =
+	// Intent.ACTION_TIME_CHANGED;
+	// date
+	// private int year;
+	// private int month;
+	private int monthDay;
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		// String action = intent.getAction();
+		// if (ACTION_TIME_CHANGED.equals(action)) {
+		// showLog("DataChange");
+		SharedPrefrenceData sharedData = new SharedPrefrenceData(context);
+		initTime();
+		int countday = sharedData.getCountDay() + 1;
+		if (monthDay == countday) {
+			sharedData.setMonthHasUsedStack(0);
+			SetText.resetWidgetAndNotify(context);
+		}
+		// }
+	}
+
+	/**
+	 * 初始化系统时间
+	 */
+	private void initTime() {
+		// Time t = new Time("GMT+8");
+		Time t = new Time();
+		t.setToNow(); // 取得系统时间。
+		// year = t.year;
+		// month = t.month + 1;
+		monthDay = t.monthDay;
+	}
+
+	private void showLog(String string) {
+		// TODO Auto-generated method stub
+		Log.d("DataChangeBroad", string);
+	}
+}
