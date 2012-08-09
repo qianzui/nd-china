ec63cf2271f9a493b6581380fb4004d789e7f313
fuweiwei0001 <fuweiwei0001@gmail.com>
2012-08-09 03:16:31 +0000

    main界面的颜色设置。

--- Spearhead/src/com/hiapk/provider/ColorChangeMainBeen.java
+++ Spearhead/src/com/hiapk/provider/ColorChangeMainBeen.java
@@ -1,5 +1,7 @@
 package com.hiapk.provider;
 
+import com.hiapk.spearhead.R;
+
 import android.graphics.Color;
 import android.widget.TextView;
 
@@ -7,6 +9,7 @@ public class ColorChangeMainBeen {
 	// 用于主界面流量显示超过特殊值时变红
 	public static int colorRed = Color.rgb(208, 31, 60);
 	public static int colorBlue = Color.rgb(64, 149, 239);
+	public static int colorDarkGray2 = Color.rgb(80, 80, 80);
 
 	/**
 	 * 用于设置本月剩余的数值以及本月已用的颜色
@@ -23,12 +26,11 @@ public class ColorChangeMainBeen {
 			tvUsed.setTextColor(Color.rgb(208, 31, 60));
 			usedTraff = 0;
 		} else {
-			tvUsed.setTextColor(Color.rgb(64, 149, 239));
+			tvUsed.setTextColor(colorDarkGray2);
 			usedTraff = mobileSet - mobileUse;
 		}
 
 		return usedTraff;
 	}
-
 
 }
