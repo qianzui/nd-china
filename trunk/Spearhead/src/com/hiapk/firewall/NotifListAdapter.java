7cdba1c2e6fb41dbce65df1eaa0b68ecab022956
fuweiwei0001@gmail.com <fuweiwei0001@gmail.com@a794dee8-fd84-c021-6015-35cb739101f1>
2012-09-20 10:03:09 +0000

    点击背景的皮肤

--- trunk/Spearhead/src/com/hiapk/firewall/NotifListAdapter.java
+++ trunk/Spearhead/src/com/hiapk/firewall/NotifListAdapter.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import com.hiapk.control.traff.TrafficManager;
 import com.hiapk.spearhead.R;
+import com.hiapk.ui.skin.SkinCustomDialog;
 import com.hiapk.util.UnitHandler;
 
 import android.content.Context;
@@ -17,56 +18,62 @@ import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class NotifListAdapter extends BaseAdapter{
-	
-	
+public class NotifListAdapter extends BaseAdapter {
+
 	private LayoutInflater inflater;
 	public Context mContext;
 	public ArrayList<String[]> notifList;
-	
-	public NotifListAdapter(Context mContext,ArrayList<String[]> notifList){
+
+	public NotifListAdapter(Context mContext, ArrayList<String[]> notifList) {
 		this.mContext = mContext;
 		this.notifList = notifList;
 		inflater = LayoutInflater.from(mContext);
 	}
-	
-	
+
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
 		ViewHolder holder;
-		if(convertView == null){
+		if (convertView == null) {
 			holder = new ViewHolder();
 			convertView = inflater.inflate(R.layout.notif_list_item, null);
-			holder.icon = (ImageView)convertView.findViewById(R.id.notif_icon);
-			holder.appname = (TextView)convertView.findViewById(R.id.notif_name);
-			holder.traffic = (TextView)convertView.findViewById(R.id.notif_traffic);
-			holder.content = (TextView)convertView.findViewById(R.id.notif_content);
-			convertView.setTag(R.id.tag_notif_holder,holder);
-		}else{
-			holder = (ViewHolder)convertView.getTag(R.id.tag_notif_holder);
+			holder.icon = (ImageView) convertView.findViewById(R.id.notif_icon);
+			holder.appname = (TextView) convertView
+					.findViewById(R.id.notif_name);
+			holder.traffic = (TextView) convertView
+					.findViewById(R.id.notif_traffic);
+			holder.content = (TextView) convertView
+					.findViewById(R.id.notif_content);
+			convertView.setTag(R.id.tag_notif_holder, holder);
+			// 设置皮肤
+			convertView.setBackgroundResource(SkinCustomDialog
+					.spinnerBackground());
+		} else {
+			holder = (ViewHolder) convertView.getTag(R.id.tag_notif_holder);
 		}
 		String[] notifRecord = notifList.get(position);
 		PackageManager pm = mContext.getPackageManager();
 		PackageInfo pkgInfo = null;
 		try {
-			pkgInfo = pm.getPackageInfo(notifRecord[0],0);
+			pkgInfo = pm.getPackageInfo(notifRecord[0], 0);
 		} catch (NameNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		if(pkgInfo == null){
+		if (pkgInfo == null) {
 			holder.appname.setText("获取失败");
 			holder.icon.setImageResource(R.drawable.ic_launcher);
-		}else{
-			holder.appname.setText(pkgInfo.applicationInfo.loadLabel(pm).toString());
-			holder.icon.setImageDrawable(pkgInfo.applicationInfo.loadIcon(pm)); 
+		} else {
+			holder.appname.setText(pkgInfo.applicationInfo.loadLabel(pm)
+					.toString());
+			holder.icon.setImageDrawable(pkgInfo.applicationInfo.loadIcon(pm));
 		}
 		int uid = pkgInfo.applicationInfo.uid;
 		long traffic[] = TrafficManager.getUidtraff(mContext, uid);
-		holder.traffic.setText("总流量: " + UnitHandler.unitHandlerAccurate(traffic[0]));
+		holder.traffic.setText("总流量: "
+				+ UnitHandler.unitHandlerAccurate(traffic[0]));
 		holder.content.setText(notifRecord[2]);
-		convertView.setTag(R.id.tag_notif_pkgInfo,pkgInfo);
-		convertView.setTag(R.id.tag_notif_id,notifRecord[1]);
+		convertView.setTag(R.id.tag_notif_pkgInfo, pkgInfo);
+		convertView.setTag(R.id.tag_notif_id, notifRecord[1]);
 		return convertView;
 	}
 
@@ -88,12 +95,11 @@ public class NotifListAdapter extends BaseAdapter{
 		return position;
 	}
 
-
-	class ViewHolder{
+	class ViewHolder {
 		ImageView icon;
 		TextView appname;
 		TextView traffic;
 		TextView content;
 	}
-	
+
 }
