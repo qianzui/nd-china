f2804917c45511bcb3cddc8e6e07bf37b77f0efc
fuweiwei0001 <fuweiwei0001@gmail.com>
2012-10-30 02:51:57 +0000

    在卸载时执行某些操作。建立函数

---
+++ Spearhead/src/com/hiapk/control/bootandclose/OnUninstallother.java
@@ -0,0 +1,17 @@
+package com.hiapk.control.bootandclose;
+
+import android.content.Context;
+
+public class OnUninstallother {
+	Context context;
+	String packagename;
+
+	public OnUninstallother(Context context, String packagename) {
+		this.context = context;
+		this.packagename = packagename;
+	}
+
+	public void operetor() {
+
+	}
+}
