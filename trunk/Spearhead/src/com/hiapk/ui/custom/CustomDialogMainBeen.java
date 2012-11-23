5e92bc7fc8d80e20e24cd7928c294cdab92490b6
fuweiwei0001@gmail.com <fuweiwei0001@gmail.com@a794dee8-fd84-c021-6015-35cb739101f1>
2012-11-23 03:54:38 +0000

    优化结构

--- trunk/Spearhead/src/com/hiapk/ui/custom/CustomDialogMainBeen.java
+++ trunk/Spearhead/src/com/hiapk/ui/custom/CustomDialogMainBeen.java
@@ -4,9 +4,7 @@ import java.text.DecimalFormat;
 
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences.Editor;
 import android.text.InputType;
-import android.text.format.Time;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -27,38 +25,12 @@ import com.hiapk.util.SharedPrefrenceData;
 import com.hiapk.util.UnitHandler;
 
 public class CustomDialogMainBeen {
-	// 操作sharedprefrence
-	private String PREFS_NAME = "allprefs";
-	// 总流量long
-	private String VALUE_MOBILE_SET = "mobilemonthuse";
-	// 设置单位（月度设置）
-	private String MOBILE_SET_UNIT = "mobileMonthUnit";
-	private String MOBILE_COUNT_SET_YEAR = "mobileMonthSetCountYear";
-	private String MOBILE_COUNT_SET_MONTH = "mobileMonthSetCountMonth";
-	private String MOBILE_COUNT_SET_DAY = "mobileMonthSetCountDay";
-	private String MOBILE_COUNT_SET_TIME = "mobileMonthSetCountTime";
-	// 已使用总流量int
-	private String VALUE_MOBILE_HASUSED_OF_FLOAT = "mobileHasusedint";
-	// 设置单位（已使用）
-	private String MOBILE_HASUSED_SET_UNIT = "mobileHasusedUnit";
-	// 已使用总流量long
-	private String VALUE_MOBILE_HASUSED_LONG = "mobileHasusedlong";
-	// 流量预警
-	private String MOBILE_WARNING_MONTH = "mobilemonthwarning";
-	private String MOBILE_WARNING_DAY = "mobiledaywarning";
 	private Context context;
-
-	// 设置时间
-	private int year;
-	private int month;
-	private int monthDay;
-	private int hour;
-	private int minute;
-	private int second;
-	private String time;
+	private SharedPrefrenceData sharedData;
 
 	public CustomDialogMainBeen(Context context) {
 		this.context = context;
+		sharedData = new SharedPrefrenceData(context);
 	}
 
 	/**
@@ -71,8 +43,6 @@ public class CustomDialogMainBeen {
 	public void dialogMonthHasUsed(final TextView monthMobil,
 			final TextView monthMobilunit, final TextView monthRemain,
 			final TextView monthRemainunit) {
-		// TODO Auto-generated method stub
-		final SharedPrefrenceData sharedData = new SharedPrefrenceData(context);
 		final DecimalFormat format = new DecimalFormat("0.##");
 		int mobileUseUnit = sharedData.getMonthHasUsedUnit();
 		// float mobileUsefloat = sharedData.getMonthMobileHasUseOffloat();
@@ -126,7 +96,6 @@ public class CustomDialogMainBeen {
 
 			@Override
 			public void onClick(View v) {
-				// TODO Auto-generated method stub
 				monthHasUsedAlert.dismiss();
 				Intent intent = new Intent(context, Regulate.class);
 				context.startActivity(intent);
@@ -139,39 +108,25 @@ public class CustomDialogMainBeen {
 
 			@Override
 			public void onClick(View v) {
-				// TODO Auto-generated method stub
 				// 输入的数值
 				float i = 0;
 				try {
 					i = Float.valueOf(et_month.getText().toString());
 				} catch (NumberFormatException e) {
-					// TODO: handle exception
 					showlog(i + "shuziError" + et_month.getText().toString());
 					i = 0;
 				}
 				// btn_Used.setText(format.format(i));
 				int mobileHasUsedUnit = spin_unit.getSelectedItemPosition();
-				Editor passfileEditor = context.getSharedPreferences(
-						PREFS_NAME, 0).edit();
-				// Log.d("main3", i + "");
-				//
-				//
 
 				if (mobileHasUsedUnit == 0) {
-					passfileEditor.putLong(VALUE_MOBILE_HASUSED_LONG,
-							(long) (i * 1048576));
 					sharedData.setMonthHasUsedStack((long) (i * 1048576));
 				} else {
-					passfileEditor.putLong(VALUE_MOBILE_HASUSED_LONG,
-							(long) (i * 1048576 * 1024));
 					sharedData
 							.setMonthHasUsedStack((long) (i * 1048576 * 1024));
 				}
-				passfileEditor.putFloat(VALUE_MOBILE_HASUSED_OF_FLOAT, i);
-
-				passfileEditor.putInt(MOBILE_HASUSED_SET_UNIT,
-						mobileHasUsedUnit);
-				passfileEditor.commit();// 委托，存入数据
+				sharedData.setMonthMobileHasUseOffloat(i);
+				sharedData.setMonthHasUsedUnit(mobileHasUsedUnit);
 				// commitUsedTrafficTime();
 				// init_btn_HasUsed();
 				/* User clicked OK so do some stuff */
@@ -184,7 +139,6 @@ public class CustomDialogMainBeen {
 				}
 				SetText.resetWidgetAndNotify(context);
 				PrefrenceStaticOperator.resetHasWarning(context);
-				commitUsedTrafficTime();
 				// 月度流量设置
 				long monthLeft = 0;
 				monthLeft = ColorChangeMainBeen.setRemainTraff(mobileSet,
@@ -207,7 +161,6 @@ public class CustomDialogMainBeen {
 
 			@Override
 			public void onClick(View v) {
-				// TODO Auto-generated method stub
 				monthHasUsedAlert.dismiss();
 			}
 		});
@@ -226,7 +179,6 @@ public class CustomDialogMainBeen {
 			final TextView monthRemainunit, final TextView monthUse,
 			final TextView monthUseunit) {
 
-		final SharedPrefrenceData sharedData = new SharedPrefrenceData(context);
 		int mobileUnit = sharedData.getMonthMobileSetUnit();
 		float mobileSetFloat = sharedData.getMonthMobileSetOfFloat();
 		// 初始化窗体
@@ -263,46 +215,33 @@ public class CustomDialogMainBeen {
 		btn_ok.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				// TODO Auto-generated method stub
 				// 输入的数值
 				float i = 0;
 				try {
 					i = Float.valueOf(et_month.getText().toString());
 				} catch (NumberFormatException e) {
-					// TODO: handle exception
 					i = 0;
 				}
 				// showlog(i + "");
 				int mobileUnit = spin_unit.getSelectedItemPosition();
-				Editor passfileEditor = context.getSharedPreferences(
-						PREFS_NAME, 0).edit();
-				// Log.d("main3", i + "");
-
 				if (mobileUnit == 0) {
 					long monthsetTraffMB = (long) (i * 1024 * 1024);
 					// showlog(monthsetTraffMB + "");
-					passfileEditor.putLong(VALUE_MOBILE_SET, monthsetTraffMB);
-					passfileEditor.putLong(MOBILE_WARNING_MONTH,
-							monthsetTraffMB * 9 / 10);
-					passfileEditor.putLong(MOBILE_WARNING_DAY,
-							monthsetTraffMB / 10);
+					sharedData.setMonthMobileSetOfLong(monthsetTraffMB);
+					sharedData.setAlertWarningMonth(monthsetTraffMB * 9 / 10);
+					sharedData.setAlertWarningDay(monthsetTraffMB / 10);
 				} else if (mobileUnit == 1) {
 					long monthsetTraffGB = (long) (i * 1024 * 1024 * 1024);
 					// showlog(monthsetTraffGB + "");
-					passfileEditor.putLong(VALUE_MOBILE_SET, monthsetTraffGB);
-					passfileEditor.putLong(MOBILE_WARNING_MONTH,
-							monthsetTraffGB * 9 / 10);
-					passfileEditor.putLong(MOBILE_WARNING_DAY,
-							monthsetTraffGB / 10);
+					sharedData.setMonthMobileSetOfLong(monthsetTraffGB);
+					sharedData.setAlertWarningMonth(monthsetTraffGB * 9 / 10);
+					sharedData.setAlertWarningDay(monthsetTraffGB / 10);
 				}
-				passfileEditor.putInt(MOBILE_SET_UNIT, mobileUnit);
+				sharedData.setMonthMobileSetUnit(mobileUnit);
 				// passfileEditor.putInt(VALUE_MOBILE_SET_OF_INT, i);
 				sharedData.setMonthMobileSetOfFloat(i);
-				passfileEditor.commit();// 委托，存入数据
 				SetText.resetWidgetAndNotify(context);
 				// 判断是否未设置
-				SharedPrefrenceData sharedData = new SharedPrefrenceData(
-						context);
 				if (i == 0) {
 					sharedData.setMonthSetHasSet(false);
 					btn_toThree.setText("  请设置流量套餐  ");
@@ -339,59 +278,9 @@ public class CustomDialogMainBeen {
 
 			@Override
 			public void onClick(View v) {
-				// TODO Auto-generated method stub
 				monthSetAlert.dismiss();
 			}
 		});
-	}
-
-	/**
-	 * 记录修改已使用流量的时间
-	 */
-	private void commitUsedTrafficTime() {
-		Editor UseEditor = context.getSharedPreferences(PREFS_NAME, 0).edit();
-		// 记录点击修改已使用流量的时间
-		String time = gettime();
-		UseEditor.putInt(MOBILE_COUNT_SET_YEAR, year);
-		UseEditor.putInt(MOBILE_COUNT_SET_MONTH, month);
-		UseEditor.putInt(MOBILE_COUNT_SET_DAY, monthDay);
-		UseEditor.putString(MOBILE_COUNT_SET_TIME, time);
-		UseEditor.commit();
-	}
-
-	/**
-	 * 获取时间
-	 * 
-	 * @return 返回00:00:00的时间
-	 */
-	private String gettime() {
-		initTime();
-		return time;
-	}
-
-	/**
-	 * 初始化系统时间
-	 */
-	private void initTime() {
-		// Time t = new Time("GMT+8");
-		Time t = new Time();
-		t.setToNow(); // 取得系统时间。
-		year = t.year;
-		month = t.month + 1;
-		monthDay = t.monthDay;
-		hour = t.hour; // 0-23
-		minute = t.minute;
-		second = t.second;
-		String hour2 = hour + "";
-		String minute2 = minute + "";
-		String second2 = second + "";
-		if (hour < 10)
-			hour2 = "0" + hour2;
-		if (minute < 10)
-			minute2 = "0" + minute2;
-		if (second < 10)
-			second2 = "0" + second2;
-		time = hour2 + ":" + minute2 + ":" + second2;
 	}
 
 	/**
