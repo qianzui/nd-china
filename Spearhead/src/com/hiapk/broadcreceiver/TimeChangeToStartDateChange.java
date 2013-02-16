10bd8ba4542b8f4aabfeddfb29ad9973cea3974b
fuweiwei0001 <fuweiwei0001@gmail.com>
2013-02-16 03:35:38 +0000

    开机时也启动每日与月度清零。

--- Spearhead/src/com/hiapk/broadcreceiver/TimeChangeToStartDateChange.java
+++ Spearhead/src/com/hiapk/broadcreceiver/TimeChangeToStartDateChange.java
@@ -11,39 +11,37 @@ import android.content.Context;
 import android.content.Intent;
 
 public class TimeChangeToStartDateChange extends BroadcastReceiver {
-	private static final String ACTION_TIME_CHANGED = Intent.ACTION_TIME_CHANGED;
+//	private static final String ACTION_TIME_CHANGED = Intent.ACTION_TIME_CHANGED;
 	private String TAG = "TimeChange";
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
 		String action = intent.getAction();
 		Logs.d(TAG, action);
-		if (ACTION_TIME_CHANGED.equals(action)) {
-			Calendar c = Calendar.getInstance();
-			c.setTimeInMillis(System.currentTimeMillis());
-			c.set(Calendar.HOUR_OF_DAY, 0);
-			c.set(Calendar.MINUTE, 0);
-			c.set(Calendar.SECOND, 0);
-			c.setTimeInMillis(c.getTimeInMillis() + 1000 * 60 * 60 * 24 - 2800);
-			long triggerTime = c.getTimeInMillis();
-			AlarmManager alarmManager = (AlarmManager) context
-					.getSystemService(Context.ALARM_SERVICE);
-			// before
-			Intent beforeIntent = new Intent(context,
-					DateChangeBroadcastBefore.class);
-			PendingIntent beforependingIntent = PendingIntent.getBroadcast(
-					context, 0, beforeIntent, 0);
-			alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, triggerTime,
-					1000 * 60 * 60 * 24, beforependingIntent);
-			// after
-			Intent afterIntent = new Intent(context,
-					DateChangeBroadcastAfter.class);
-			PendingIntent afterpendingIntent = PendingIntent.getBroadcast(
-					context, 0, afterIntent, 0);
-			alarmManager
-					.setRepeating(AlarmManager.RTC_WAKEUP, triggerTime + 3000,
-							1000 * 60 * 60 * 24, afterpendingIntent);
-		}
+		// if (ACTION_TIME_CHANGED.equals(action)) {
+		Calendar c = Calendar.getInstance();
+		c.setTimeInMillis(System.currentTimeMillis());
+		c.set(Calendar.HOUR_OF_DAY, 0);
+		c.set(Calendar.MINUTE, 0);
+		c.set(Calendar.SECOND, 0);
+		c.setTimeInMillis(c.getTimeInMillis() + 1000 * 60 * 60 * 24 - 2800);
+		long triggerTime = c.getTimeInMillis();
+		AlarmManager alarmManager = (AlarmManager) context
+				.getSystemService(Context.ALARM_SERVICE);
+		// before
+		Intent beforeIntent = new Intent(context,
+				DateChangeBroadcastBefore.class);
+		PendingIntent beforependingIntent = PendingIntent.getBroadcast(context,
+				0, beforeIntent, 0);
+		alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, triggerTime,
+				1000 * 60 * 60 * 24, beforependingIntent);
+		// after
+		Intent afterIntent = new Intent(context, DateChangeBroadcastAfter.class);
+		PendingIntent afterpendingIntent = PendingIntent.getBroadcast(context,
+				0, afterIntent, 0);
+		alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, triggerTime + 3000,
+				1000 * 60 * 60 * 24, afterpendingIntent);
+		// }
 	}
 
 	// private void showLog(String string) {
